74b21ea000beee0af1aeffe010784b7594692c48
Drantho <drantho@gmail.com>
2021-01-18 18:23:00 -0800

    route post api/notes added to write file db/db.json

--- server.js
+++ server.js
@@ -25,6 +25,29 @@ app.get("/api/notes", (req, res)=>{
     })
 })
 
+app.post("/api/notes", (req, res) => {
+    console.log(req.body);
+
+    fs.readFile(path.join(__dirname, "./db/db.json"), "utf-8",(err, file) => {
+        if(err){
+            throw(err)
+        }
+
+        const data = JSON.parse(file);
+
+        data.push(req.body);
+
+        fs.writeFile(path.join(__dirname, "./db/db.json"), JSON.stringify(data), err => {
+            if(err){
+                throw(err)
+            }
+
+            res.status(200).json(data);
+        })
+        
+    })
+})
+
 app.get("*", (req, res) => {
     res.sendFile(path.join( __dirname, "./public.index.html"));
 })
