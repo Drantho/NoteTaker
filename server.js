6dabb4b44966b0e300f06748d2ff7db4baed15e1
Drantho <drantho@gmail.com>
2021-01-18 17:56:53 -0800

    route / updated to serv index.html

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 const express = require("express");
+const path = require( "path" );
 
 const app = express();
 
 const PORT = process.env.PORT || 3000;
 
-app.get("/", (req, res) => {
-    res.end("main page");
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use( express.static( "public" ));
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join( __dirname, "./public.index.html"));
 })
 
 app.listen(PORT, ()=>{
