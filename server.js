331d9559b0b45a04afdd9a8a916e779dec00d86c
Drantho <drantho@gmail.com>
2021-01-18 18:12:05 -0800

    route api/notes added to serve db.json

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const path = require( "path" );
+const fs = require("fs");
 
 const app = express();
 
@@ -12,6 +13,16 @@ app.use( express.static( "public" ));
 
 app.get("/notes", (req, res) =>{
     res.sendFile(path.join( __dirname, "./public/notes.html"));
+})
+
+app.get("/api/notes", (req, res)=>{
+    fs.readFile(path.join(__dirname, "./db/db.json"), "utf-8",(err, data) => {
+        if(err){
+            throw(err)
+        }
+        
+        res.status(200).json(JSON.parse(data))
+    })
 })
 
 app.get("*", (req, res) => {
